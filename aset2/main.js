b481491577b79365061d939eb367b8c3bcb2c71b
arifkratoss <arifkratos21@gmail.com>
2023-05-06 08:57:31 -0800

    merevisi 23-05-23

--- aset2/main.js
+++ aset2/main.js
@@ -3,19 +3,3 @@ let menuOpen =document.querySelector('.menu-toggle')
 menuOpen.addEventListener('click',function (){
 
 })
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
