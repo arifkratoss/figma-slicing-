8ada4f7a1cb0845d0131b44a657a7ce00fcf8896
arifkratoss <arifkratos21@gmail.com>
2023-05-06 18:12:31 -0800

    final dari revisi

--- aset2/main.js
+++ aset2/main.js
@@ -3,3 +3,8 @@ let menuOpen =document.querySelector('.menu-toggle')
 menuOpen.addEventListener('click',function (){
 
 })
+var swiper = new Swiper(".mySwiperTesti", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
